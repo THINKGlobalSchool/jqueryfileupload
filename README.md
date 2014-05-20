75adb546bd58c0553bf7e981aab1d0f17ab77a17
Jeff Tilson <jrtilson@gmail.com>
2014-05-20 12:37:05 -0400

    Added js files, manifest

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-jqueryfileupload
+Elgg - jQuery File Upload
 ================
 
 Provide jQuery-File-Upload JS to Elgg plugins
+
+See: https://github.com/blueimp/jQuery-File-Upload
---
+++ start.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Elgg jQuery File Upload
+ *
+ * @package jqueryfileupload
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jeff Tilson
+ * @copyright THINK Global School 2010 - 2014
+ * @link http://www.thinkglobalschool.com/
+ *
+ */
+
+elgg_register_event_handler('init', 'system', 'jfu_init');
+
+function jfu_init() {
+	// Register cachable views
+	elgg_register_external_view('js/jquery.iframe-transport', true);
+	elgg_register_external_view('js/jquery.fileupload', true);
+
+	return TRUE;
+}
